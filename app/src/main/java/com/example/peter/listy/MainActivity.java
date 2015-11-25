4bb6e3cf8526c82944b3c38558627f07602fe959
Peter Hoggarth <hoggarth.peter@gmail.com>
2015-11-25 16:26:09 -0500

    Added items to the navigation tray

--- app/src/main/java/com/example/peter/listy/MainActivity.java
+++ app/src/main/java/com/example/peter/listy/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.peter.listy;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -23,15 +24,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -40,6 +32,12 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.getMenu().clear();
+        navigationView.getMenu().add("Add Recipe");
+        navigationView.getMenu().add("Set Budget");
+        navigationView.getMenu().add("Find Location");
+        navigationView.getMenu().add("Clear All");
+
     }
 
     @Override
